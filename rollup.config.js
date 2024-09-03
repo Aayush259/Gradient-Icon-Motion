f95a94889e5e05e0f444cda816dc843bf5b518b5
Square <aayushk4980@gmail.com>
2024-09-03 16:50:37 +0530

    Added rollup config file

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            resolve({
+                include: ['node_modules/**'],
+            }),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+            postcss({
+                extract: true,  // Extract CSS into a separate file
+                minimize: false, // Minify the CSS
+            }),
+            terser(),
+        ],
+        external: ['react', 'react-dom'],
+    },
+    {
+        input: "dist/esm/types/index.d.ts",
+        output: [{ file: "dist/index.d.ts", format: "esm" }],
+        plugins: [dts.default()],
+        external: [/\.css$/],
+    },
+];
