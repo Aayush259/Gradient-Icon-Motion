041102ef3546d4512cad8476f680be99e17b270e
Square <aayushk4980@gmail.com>
2024-09-03 23:08:49 +0530

    Added animation width and height controls in props and update animation positioning in GradientIconMotion component

--- src/components/GradientIconMotion.tsx
+++ src/components/GradientIconMotion.tsx
@@ -3,7 +3,7 @@ import { BackgroundGradientAnimation } from './BackgroundGradient';
 import { FloatingIcons } from './FloatingIcons';
 import { FloatingIconsProps, BackgroundGradientAnimationProps, CommonProps } from '../interface/props';
 
-interface GradientIconMotionProps extends FloatingIconsProps, BackgroundGradientAnimationProps, CommonProps {}
+interface GradientIconMotionProps extends FloatingIconsProps, BackgroundGradientAnimationProps, CommonProps { }
 
 const GradientIconMotion: React.FC<GradientIconMotionProps> = ({
     gradientBackgroundStart,
@@ -18,30 +18,43 @@ const GradientIconMotion: React.FC<GradientIconMotionProps> = ({
     floatDirectionReverse,
     iconColor,
     icons,
+    animationHeight,
+    animationWidth,
     children,
 }) => {
     return (
         <>
-            <div>
-                <div className="relative z-50">
+            <div className="relative">
+                <div>
                     {children}
                 </div>
-                <BackgroundGradientAnimation
-                    gradientBackgroundEnd={gradientBackgroundEnd}
-                    gradientBackgroundStart={gradientBackgroundStart}
-                    firstColor={firstColor}
-                    secondColor={secondColor}
-                    thirdColor={thirdColor}
-                    fourthColor={fourthColor}
-                    fifthColor={fifthColor}
-                    size={size}
-                    blendingValue={blendingValue}
-                />
-                <FloatingIcons
-                    floatDirectionReverse={floatDirectionReverse}
-                    iconColor={iconColor}
-                    icons={icons}
-                />
+
+                <div className="absolute top-0 left-0">
+                    <BackgroundGradientAnimation
+                        gradientBackgroundEnd={gradientBackgroundEnd}
+                        gradientBackgroundStart={gradientBackgroundStart}
+                        firstColor={firstColor}
+                        secondColor={secondColor}
+                        thirdColor={thirdColor}
+                        fourthColor={fourthColor}
+                        fifthColor={fifthColor}
+                        size={size}
+                        blendingValue={blendingValue}
+                        animationHeight={animationHeight}
+                        animationWidth={animationWidth}
+                    />
+                </div>
+
+
+                <div className="absolute top-0 left-0">
+                    <FloatingIcons
+                        floatDirectionReverse={floatDirectionReverse}
+                        iconColor={iconColor}
+                        icons={icons}
+                        animationHeight={animationHeight}
+                        animationWidth={animationWidth}
+                    />
+                </div>
             </div>
         </>
     );
